803e3414c6a1ec6753cc6ccd1434cabd54f6ae90
Pablo Barcala <pablobarcala4@gmail.com>
2023-09-26 17:16:36 -0300

    agregar primera funcionalidad de prueba al service

--- src/app/services/persona.service.ts
+++ src/app/services/persona.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonaService {
+  apiUrl: string = "http://66.97.47.250:8080"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSaludo() {
+    return this.http.get(this.apiUrl + "/getSaludo")
+  }
 }
