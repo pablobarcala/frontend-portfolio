17aa4e79c3dbf0b06028e21a54986b097269fcbc
Pablo Barcala <pablobarcala4@gmail.com>
2023-09-26 17:20:25 -0300

    agregar service al header solo para probar su conexion

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PersonaService } from 'src/app/services/persona.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  saludo: string = ""
+  
+  constructor(private personaService: PersonaService){
+    personaService.getSaludo().subscribe((resp: any) => this.saludo = resp.mensaje)
+  }
 }
