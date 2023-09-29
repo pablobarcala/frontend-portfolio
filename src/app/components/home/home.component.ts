6095d818dc549fbb0291da1bd00221dd41338351
Pablo Barcala <pablobarcala4@gmail.com>
2023-09-29 11:22:14 -0300

    llevar el cursor al home y sacarlo del header

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+    this.seguirCursor()
+  }
+
+  seguirCursor() {
+    document.addEventListener("mousemove", (event: MouseEvent) => {
+      const follower = document.getElementById("follower") as HTMLElement
+      const x = event.clientX
+      const y = event.clientY
+
+      if(follower){
+        follower.style.display = "block"
+        follower.style.left = x + "px"
+        follower.style.top = y + "px"
+      }
+    })
+  }
 }
