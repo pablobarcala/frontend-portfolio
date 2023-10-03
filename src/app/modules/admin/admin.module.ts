3be1708652021b429f5cdad1fed7cd629329b312
Pablo Barcala <pablobarcala4@gmail.com>
2023-10-03 15:24:37 -0300

    estilizar un poco el login y agregar routing al modulo admin

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
+import { RoutingModule } from '../routing/routing.module';
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RoutingModule
   ]
 })
 export class AdminModule { }
