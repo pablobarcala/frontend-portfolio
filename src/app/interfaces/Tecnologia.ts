3765061aa6b702deac4d8c983816e2f8dc86b013
Pablo Barcala <pablobarcala4@gmail.com>
2023-10-03 22:53:07 -0300

    agregar interface y mock de tecnologias

---
+++ src/app/interfaces/Tecnologia.ts
@@ -0,0 +1,5 @@
+export interface Tecnologia {
+    id?: number,
+    nombreTecnologia?: string,
+    porcentaje?: number
+}
---
+++ src/app/interfaces/mock-tecnologia.ts
@@ -0,0 +1,9 @@
+import { Tecnologia } from "./Tecnologia";
+
+export const Tecnologias: Tecnologia[] = [
+    {
+        id: 0,
+        nombreTecnologia: "HTML",
+        porcentaje: 90
+    }
+]
