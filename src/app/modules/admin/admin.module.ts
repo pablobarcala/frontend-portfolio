2fe8b77ae4b8a5f6aa3fca302fd6e26955321fde
Pablo Barcala <pablobarcala4@gmail.com>
2023-10-12 17:40:24 -0300

    agregar dashboard y sacar cursor en celular

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { RoutingModule } from '../routing/routing.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule,
