074d6069fe274443e99f55802972febd9194c199
Pablo Barcala <pablobarcala4@gmail.com>
2023-10-12 20:32:33 -0300

    agregar panel de control inicial, con header

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { RoutingModule } from '../routing/routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    AdminComponent
   ],
   imports: [
     CommonModule,
--- src/app/modules/routing/routing.module.ts
+++ src/app/modules/routing/routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../admin/components/login/login.component';
 import { HomeComponent } from 'src/app/components/home/home.component';
+import { DashboardComponent } from '../admin/components/dashboard/dashboard.component';
+import { AdminComponent } from '../admin/components/admin/admin.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'admin', component: LoginComponent}
+  {path: 'admin', component: AdminComponent, children: [
+    {path: 'login', component: LoginComponent},
+    {path: 'panel', component: DashboardComponent}
+  ]}
 ]
 
 
