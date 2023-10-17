d2eead90f99a2b17dca48b3216e07de1614fb22b
Pablo Barcala <pablobarcala4@gmail.com>
2023-10-17 08:35:15 -0300

    agregar link de contacto de persona a contacto

--- src/app/components/contacto-item/contacto-item.component.ts
+++ src/app/components/contacto-item/contacto-item.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Persona } from 'src/app/interfaces/Persona';
+import { PersonaService } from 'src/app/services/persona.service';
 
 @Component({
   selector: 'app-contacto-item',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto-item.component.css']
 })
 export class ContactoItemComponent {
+  persona: Persona | undefined = undefined;
 
+  constructor(
+    private personaService: PersonaService
+  ){
+    personaService.getPersonas().subscribe((resp: any) => {
+      let personas: Persona[] = resp
+
+      personas.find((p: any) => {
+        if(p.idpersona == 1){
+          this.persona = p
+        }
+      })
+    })
+  }
 }
