23163e50953b2e34e9be6af6cd93859f0bbd4c63
pablobarcala <pablobarcala4@gmail.com>
2023-11-28 10:15:59 -0300

    agregar form para imagen en add tecnologias

--- src/app/modules/admin/components/add-tecnologias/add-tecnologias.component.ts
+++ src/app/modules/admin/components/add-tecnologias/add-tecnologias.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage, getDownloadURL, ref, uploadBytes } from '@angular/fire/storage';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TecnologiaService } from 'src/app/services/tecnologia.service';
@@ -10,15 +11,32 @@ import { TecnologiaService } from 'src/app/services/tecnologia.service';
 })
 export class AddTecnologiasComponent {
   form: FormGroup
+  editarFoto: boolean = false
+  imagenUrl: string = ""
 
   constructor(
     private formBuilder: FormBuilder,
     private tecnologiaService: TecnologiaService,
-    private router: Router
+    private router: Router,
+    private storage: Storage
   ) {
     this.form = formBuilder.group({
       nombreTecnologia: ['', Validators.required],
-      porcentaje: [0, Validators.required]
+      porcentaje: [0, Validators.required],
+      imagen: ['']
+    })
+  }
+
+  getImage(event: any){
+    this.editarFoto = true
+    const file = event.target.files[0]
+
+    const imgRef = ref(this.storage, `tecnologias/${file.name}`)
+
+    uploadBytes(imgRef, file)
+    .then(async () => {
+      this.imagenUrl = await getDownloadURL(imgRef)
+      alert("Imagen guardada")
     })
   }
 
@@ -26,15 +44,32 @@ export class AddTecnologiasComponent {
     event.preventDefault()
 
     if(this.form.valid){
-      this.tecnologiaService.addTecnologia(this.form.value).subscribe((resp: any) => {
-        if(resp){
-          alert("Se creó correctamente")
-          this.router.navigate(['/admin/panel/tecnologias'])
-          .then(() => window.location.reload())
-        } else {
-          alert("Hubo algún error")
-        }
-      })
+      if(this.imagenUrl != '' && this.editarFoto){
+        this.form.patchValue({
+          imagen: this.imagenUrl
+        })
+        this.tecnologiaService.editTecnologia(this.form.get('id')?.value, this.form.value).subscribe((resp: any) => {
+          if(resp){
+            alert("Se creó correctamente")
+            this.router.navigate(['/admin/panel/tecnologias'])
+            .then(() => window.location.reload())
+          } else {
+            alert("Hubo algún error")
+          }
+        })
+      } else if(!this.editarFoto){
+        this.tecnologiaService.editTecnologia(this.form.get('id')?.value, this.form.value).subscribe((resp: any) => {
+          if(resp){
+            alert("Se creó correctamente")
+            this.router.navigate(['/admin/panel/tecnologias'])
+            .then(() => window.location.reload())
+          } else {
+            alert("Hubo algún error")
+          }
+        })
+      } else {
+        alert("Espere")
+      }
     } else {
       this.form.markAllAsTouched()
     }
