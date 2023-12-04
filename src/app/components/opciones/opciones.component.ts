347d76300414937fb21cc10ce0521adb274bb55c
pablobarcala <pablobarcala4@gmail.com>
2023-12-04 14:10:30 -0300

    sacar iconos de redes del celular y agregrlos al menu, ademas cambiar de lugar el icono de tema en header del celular

--- src/app/components/opciones/opciones.component.ts
+++ src/app/components/opciones/opciones.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Persona } from 'src/app/interfaces/Persona';
 import { NavService } from 'src/app/services/nav.service';
+import { PersonaService } from 'src/app/services/persona.service';
 
 @Component({
   selector: 'app-opciones',
@@ -8,10 +10,22 @@ import { NavService } from 'src/app/services/nav.service';
 })
 export class OpcionesComponent {
   opciones: string[] = ["Inicio", "Proyectos", "Experiencia", "Tecnologías", "Educacion", "Contacto"];
+  persona: Persona | null = null
 
   constructor(
-    private navService: NavService
-  ){}
+    private navService: NavService,
+    private personaService: PersonaService
+  ){
+    personaService.getPersonas().subscribe((resp: any) => {
+      let personas: Persona[] = resp
+
+      personas.find((p: any) => {
+        if(p.id == 1){
+          this.persona = p
+        }
+      })
+    })
+  }
 
   closeMenu(){
     this.navService.toggleMenu()
